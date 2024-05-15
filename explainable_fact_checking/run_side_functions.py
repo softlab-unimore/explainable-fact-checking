0a4381ac4140f87682ea9b9fd7438f68830d2920
Andrea <baraldian@gmail.com>
2024-05-15 17:16:24 +0200

    add: run file to map_evidence_types. Not working.

--- explainable_fact_checking/run_side_functions.py
+++ explainable_fact_checking/run_side_functions.py
@@ -1,7 +1,8 @@
+import json
 import os
 
-from explainable_fact_checking import FeverousModelAdapter, AddInputTxtToUse
-from explainable_fact_checking import save_prediciton_without_evidence
+from explainable_fact_checking import FeverousModelAdapter
+import explainable_fact_checking as xfc
 
 if __name__ == "__main__":
     fc_model = FeverousModelAdapter()
@@ -12,20 +13,25 @@ if __name__ == "__main__":
     # input_file = '/homes/bussotti/feverous_work/feverousdata/feverous_dev_challenges_sentencesonly.jsonl'
     # output_file = os.path.join(AB_path, 'feverous_dev_SO_01.jsonl')
     for input_file_name in [
-
+        'feverous_dev_ST_01.jsonl',
+        'ex_AB_00.jsonl',
         'original_TO_01.jsonl',
-        # 'ex_AB_00.jsonl',
-        # 'feverous_dev_ST_01.jsonl',
-        # 'feverous_dev_SO_01.jsonl'
+        'feverous_dev_SO_01.jsonl'
     ]:
-        input_file_path = os.path.join(AB_path, input_file_name)
-        output_file = os.path.join(AB_path, input_file_name.replace('.jsonl', '_pred_only_claim.json'))
-        save_prediciton_without_evidence(input_file_path, output_file=output_file, model=fc_model)
+        input_file = os.path.join(AB_path, input_file_name)
+        output_file = os.path.join(AB_path, input_file_name.replace('.jsonl', '_evidence_type_map.pickle'))
+        with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
+            record_list = [json.loads(line) for line in f_in]
+            evidence_type_map = xfc.xfc_utils.map_evidence_types(record_list)
+            f_out.write(json.dumps(evidence_type_map) + '\n')
 
-    # input_file = '/homes/bussotti/feverous_work/feverousdata/AB/ex_for_andrea_rightformat.jsonl'
-    # output_file = os.path.join(AB_path, 'ex_AB_00.jsonl')
-    # AddInputTxtToUse.predict_and_save(fc_model, input_file=input_file,
-    #                                   output_file=output_file)
+        # output_file = os.path.join(AB_path, input_file_name.replace('.jsonl', '_pred_only_claim.json'))
+        # # xfc.xfc_utils.save_prediciton_without_evidence(input_file_path, output_file=output_file, model=fc_model)
+
+    # input_file = os.path.join(AB_path, 'original_TO_01.jsonl')
+    # output_file = os.path.join(AB_path, 'original_TO_01_formatted.jsonl')
+    # xfc.xfc_utils.AddInputTxtToUse.predict_and_save(input_file=input_file,
+    #                                   output_file=output_file, model=fc_model)
 
     # launch a shell command to compress folders in the AB/lime_explanations folder
     # os.system(f'cd {AB_path} && tar -czf lime_explanations.tar.gz lime_explanations')
