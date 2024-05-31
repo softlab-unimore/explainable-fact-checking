f07178f6c422cf4c6694fd30fb113bb07e197e9d
Andrea <baraldian@gmail.com>
2024-05-31 17:42:02 +0200

    add: catching errors and saying total time in logger; improved remotion of old results; introduced explain_list function that let the explainer explain in bath multiple elements in batch.

--- explainable_fact_checking/experiment_routine.py
+++ explainable_fact_checking/experiment_routine.py
@@ -2,22 +2,27 @@ import gc
 import itertools
 import json
 import pickle
+import shutil
+import sys
 from collections.abc import Iterable
 from datetime import datetime
 
-import send2trash
 from tqdm import tqdm
 
 import explainable_fact_checking as xfc
 import os
 
+print("CUDA_VISIBLE_DEVICES:", os.environ.get("CUDA_VISIBLE_DEVICES"))
+print("CUDA_DEVICE_ORDER:", os.environ.get("CUDA_DEVICE_ORDER"))
+import torch
 
-
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+print("CUDA is available:", torch.cuda.is_available())
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # Root directory
 base_path = '/homes/bussotti/feverous_work/feverousdata'
+
 
 class ExperimentRunner:
 
@@ -25,7 +30,6 @@ class ExperimentRunner:
     def product_dict(**kwargs):
         keys = kwargs.keys()
         return [dict(zip(keys, instance)) for instance in itertools.product(*kwargs.values())]
-
 
     def launch_experiment_by_config(self, exp_dict: dict):
         if exp_dict is None:
@@ -68,27 +72,39 @@ class ExperimentRunner:
         os.makedirs(results_dir, exist_ok=True)
         logger = xfc.xfc_utils.init_logger(save_dir=results_dir)
         logger.info(f"Starting experiment {experiment_id}")
+        start_time = datetime.now()
         for i, kwargs in tqdm(enumerate(self.product_dict(**params_dict_to_iterate))):
             gc.collect()
-            turn_a = datetime.now()
             logger.info(f"Iteration: {i} with parameters: {kwargs}")
             kwargs['results_dir'] = os.path.join(results_dir, str(i))
-            self.launch_single_experiment(**kwargs, logger=logger)
+            turn_a = datetime.now()
+
+            try:
+                self.launch_single_experiment(**kwargs, logger=logger)
+            except Exception as e:
+                logger.error(f"Error in iteration {i}.")
+                logger.error(e)
+                gettrace = getattr(sys, 'gettrace', None)
+                if gettrace is None:
+                    pass
+                elif gettrace():
+                    raise e
             turn_b = datetime.now()
             logger.info(f"Iteration: {i} completed in {turn_b - turn_a}")
-
-
+        end_time = datetime.now()
+        logger.info(f"Experiment {experiment_id} completed in {end_time - start_time}")
 
     def launch_single_experiment(self, experiment_id, dataset_name, results_dir, random_seed, model_name, model_params,
                                  explainer_name, dataset_params,
                                  explainer_params, logger, **kwargs):
         try:
-            os.makedirs(results_dir, exist_ok=True)
-            for filepath in os.scandir(results_dir):
-                send2trash.send2trash(filepath)
+            if os.path.exists(results_dir):
+                shutil.rmtree(results_dir + '.old', ignore_errors=True)
+                shutil.move(results_dir, results_dir + '.old')
+
         except FileNotFoundError:
             pass
-
+        os.makedirs(results_dir, exist_ok=True)
         base_experiment_params = dict(experiment_id=experiment_id, dataset_name=dataset_name,
                                       random_seed=random_seed, model_name=model_name, model_params=model_params,
                                       explainer_name=explainer_name, dataset_params=dataset_params,
@@ -98,51 +114,61 @@ class ExperimentRunner:
 
         dataset = xfc.datasets_loaders.dataset_loader_factory.create(dataset_name, **dataset_params)
         model = xfc.models.model_factory.create(model_name, random_state=random_seed, **model_params)
-        explainer = xfc.explainers.explainer_factory.create(explainer_name, **explainer_params) # todo
+        explainer = xfc.explainers.explainer_factory.create(explainer_name, **explainer_params)  # todo
         exp_list = []
-        for i, record in tqdm(enumerate(dataset)):
-            exp = explainer.explain(record=record, predictor=model.predict)
-            exp_list.append(exp)
-            if i % 10 == 0:
-                with open(os.path.join(results_dir, 'explanation_list.pkl'), 'wb') as file:
-                    pickle.dump(exp_list, file)
+        if hasattr(explainer, 'explain_list'):
+            try:
+                exp_list = explainer.explain_list(record_list=dataset, predict_method=model)
+            except Exception as e:
+                xfc.xfc_utils.handle_exception(e, logger)
+        else:
+            for i, record in tqdm(enumerate(dataset)):
+                try:
+                    exp = explainer.explain(record=record, predict_method=model)
+                    exp_list.append(exp)
+                except Exception as e:
+                    logger.error(f"Error in record {i}")
+                    xfc.xfc_utils.handle_exception(e, logger)
+
+                if i % 10 == 0:
+                    with open(os.path.join(results_dir, 'explanation_list.pkl'), 'wb') as file:
+                        pickle.dump(exp_list, file)
         with open(os.path.join(results_dir, 'explanation_list.pkl'), 'wb') as file:
             pickle.dump(exp_list, file)
 
+        # remove result 'old' directory given that experiment is done
+        if os.path.exists(results_dir + '.old'):
+            shutil.rmtree(results_dir + '.old')
+
+
+experiment_done = [
+
+]
+
+experiments_doing = [
+    'sk_f_jf_1.0',
+    'sk_f_jf_1.1',
+    'sk_f_jf_1.1b',
+    'sk_f_jf_1.1n',
+    'f_bs_1.0',
+    'f_bs_1.1',
+
+    # only claim
+    'oc_1.0',
+    'oc_1.1',
+]
 
 # main thing to start the experiment
 if __name__ == "__main__":
-    experiment_done = [
-        'sk_f_jf_1.0',
-    ]
 
     experiments_to_run = [
 
-        'sk_f_jf_1.1',
-        ]
+    ]
 
     experiment_runner = ExperimentRunner()
     for exp_id in experiments_to_run:
         if exp_id in experiment_done:
+            print(f"Experiment {exp_id} already done, skipping...")
             continue
         exp_dict = xfc.experiment_definitions.get_config_by_id(exp_id)
         experiment_runner.launch_experiment_by_config(exp_dict)
-
-    # fc_model = FeverousModelAdapter()
-    # predictor = fc_model.predict
-    # for name in [
-    #     'original_TO_01_formatted.jsonl',
-    #     # 'ex_AB_00.jsonl',
-    #     # 'feverous_dev_ST_01.jsonl',
-    #     # 'feverous_dev_SO_01.jsonl',
-    # ]:
-    #     input_file_path = os.path.join('/homes/bussotti/feverous_work/feverousdata/AB/', name)
-    #
-    #     explainable_fact_checking.explainers.explain_with_SHAP(input_file_path, predictor=predictor,
-    #                                                            output_dir='/homes/bussotti/feverous_work/feverousdata/AB/SHAP_explanations',
-    #                                                            num_samples=500,
-    #                                                            top=1000, mode='KernelExplainer')
-    #
-    #     # xfc.wrappers.explain_with_lime(input_file_path, predictor=predictor,
-    #     #                   output_dir='/homes/bussotti/feverous_work/feverousdata/AB/lime_explanations', num_samples=500,
-    #     #                   top=1000)
