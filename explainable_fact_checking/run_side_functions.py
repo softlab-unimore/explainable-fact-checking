a5c5cac2648c5259109f26e75221f43853c9c812
Andrea <baraldian@gmail.com>
2024-05-31 18:07:18 +0200

    ref: use static values for path; adding text input for noisy dataset.

--- explainable_fact_checking/run_side_functions.py
+++ explainable_fact_checking/run_side_functions.py
@@ -5,37 +5,32 @@ from explainable_fact_checking import FeverousModelAdapter
 import explainable_fact_checking as xfc
 
 if __name__ == "__main__":
-    fc_model = FeverousModelAdapter()
+    fc_model = FeverousModelAdapter(model_path=xfc.experiment_definitions.C.JF_feverous_model['model_params']['model_path'][0])
     predictor = fc_model.predict
-    AB_path = '/homes/bussotti/feverous_work/feverousdata/AB/'
-    # input_file = os.path.join('/homes/bussotti/feverous_work/feverousdata/', 'feverous_dev_challenges_sentencesandtable.jsonl')
-    # output_file = os.path.join(AB_path, 'feverous_dev_ST_01.jsonl')
-    # input_file = '/homes/bussotti/feverous_work/feverousdata/feverous_dev_challenges_sentencesonly.jsonl'
-    # output_file = os.path.join(AB_path, 'feverous_dev_SO_01.jsonl')
+    AB_path = xfc.experiment_definitions.C.DATASET_DIR_FEVEROUS[0]
+
+
+    # res_wrong = xfc.explanations_load.load_experiment_result_by_code('sk_f_jf_1.1', results_path=xfc.experiment_definitions.C.RESULTS_DIR)
     for input_file_name in [
-        'feverous_dev_ST_01.jsonl',
-        'ex_AB_00.jsonl',
-        'original_TO_01.jsonl',
-        'feverous_dev_SO_01.jsonl'
+        'feverous_train_challenges_withnoise.jsonl',
+        #'feverous_dev_ST_01.jsonl',
+        #'ex_AB_00.jsonl',
+        #'original_TO_01.jsonl',
+        #'feverous_dev_SO_01.jsonl'
     ]:
         input_file = os.path.join(AB_path, input_file_name)
-        output_file = os.path.join(AB_path, input_file_name.replace('.jsonl', '_evidence_type_map.pickle'))
-        with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
-            record_list = [json.loads(line) for line in f_in]
-            evidence_type_map = xfc.xfc_utils.map_evidence_types(record_list)
-            f_out.write(json.dumps(evidence_type_map) + '\n')
+        # output_file = os.path.join(AB_path, input_file_name.replace('.jsonl', '_evidence_type_map.pickle'))
+        # with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
+        #     record_list = [json.loads(line) for line in f_in]
+        #     evidence_type_map = xfc.xfc_utils.map_evidence_types(record_list)
+        #     f_out.write(json.dumps(evidence_type_map) + '\n')
 
         # output_file = os.path.join(AB_path, input_file_name.replace('.jsonl', '_pred_only_claim.json'))
-        # # xfc.xfc_utils.save_prediciton_without_evidence(input_file_path, output_file=output_file, model=fc_model)
+        # xfc.xfc_utils.save_prediciton_only_claim(input_file, output_file=output_file, model=fc_model)
+
+        xfc.xfc_utils.AddInputTxtToUse.predict_and_save(input_file=input_file,
+                                                        output_file=input_file, model=fc_model)
 
     # input_file = os.path.join(AB_path, 'original_TO_01.jsonl')
     # output_file = os.path.join(AB_path, 'original_TO_01_formatted.jsonl')
-    # xfc.xfc_utils.AddInputTxtToUse.predict_and_save(input_file=input_file,
-    #                                   output_file=output_file, model=fc_model)
 
-    # launch a shell command to compress folders in the AB/lime_explanations folder
-    # os.system(f'cd {AB_path} && tar -czf lime_explanations.tar.gz lime_explanations')
-    # explain the parameters of tar
-    # c: create a new archive
-    # z: compress the archive with gzip
-    # f: use the given archive file
