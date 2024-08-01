b9806d21ac5977fcd7fe4350c132cb4fb3b59a08
Andrea <baraldian@gmail.com>
2024-08-01 15:57:37 +0200

    add: refactor load_preprocess_explanations, introduced params to specify exp to load, added assert to double check noisetag len, moved into plot as it's used for visualization.

--- explainable_fact_checking/plot/__init__.py
+++ explainable_fact_checking/plot/__init__.py
@@ -1,1 +1,2 @@
 from . import style
+from . import explanations_loader
--- explainable_fact_checking/plot/explanations_loader.py
+++ explainable_fact_checking/plot/explanations_loader.py
@@ -1,9 +1,7 @@
 import json
 import os
 import pickle
-
 import pandas as pd
-
 import explainable_fact_checking as xfc
 
 
@@ -53,7 +51,7 @@ def explanations_to_df_lime(explanations_files):
     return ret_df.apply(pd.to_numeric, errors='ignore')
 
 
-def explanations_to_df(explanations_list: list):
+def explanations_to_df(explanation_object_list: list):
     """
     Create a pandas dataframe from the explanations
     input: explanations list, where each element is a pair of (params_dict, explanation_list)
@@ -68,7 +66,7 @@ def explanations_to_df(explanations_list: list):
 
     """
     out_list = []
-    for params_dict, explanation_list in explanations_list:
+    for params_dict, explanation_list in explanation_object_list:
         for explanation in explanation_list:
             # tdict = params_dict.copy()
             # the params dict has sub dictionaries, we need to flatten it
@@ -103,6 +101,7 @@ def explanation_to_dict_olap(exp):
         get_method = lambda x: exp.get(x, None)
     else:
         get_method = lambda x: getattr(exp, x) if hasattr(exp, x) else None
+    assert get_method('record').get('noisetag') is None or len(get_method('record')['noisetag']) == len(get_method('local_exp')[0])
     ret_list = []
     out_dict = {}
     for key in ['num_samples', 'label', 'execution_time', 'id']:
@@ -199,30 +198,18 @@ def load_only_claim_predictions(dir='/homes/bussotti/feverous_work/feverousdata/
     return pd.DataFrame(out_list)
 
 
-def load_preprocess_explanations():
-    exp_list = []
-    for experiment_code in [
-        'sk_f_jf_1.1',
-        'sk_f_jf_1.0',
-        'sk_f_jf_1.1b',
-        'sk_f_jf_1.1n',
-        'f_bs_1.0',
-        'f_bs_1.1',
-        'f_bs_1.1b',
-    'f_bs_1.1c',
-    ]:
+def load_preprocess_explanations(experiment_code_list: list, only_claim_exp_list: list):
+    exp_object_list = []
+    for experiment_code in experiment_code_list:
         x = load_experiment_result_by_code(experiment_code, xfc.experiment_definitions.C.RESULTS_DIR)
-        exp_list += x
+        exp_object_list += x
 
     only_claim_list = []
-    for experiment_code in [
-        'oc_1.0',
-        'oc_1.1',
-    ]:
+    for experiment_code in only_claim_exp_list:
         only_claim = load_experiment_result_by_code(experiment_code, xfc.experiment_definitions.C.RESULTS_DIR)
         only_claim_list += only_claim
 
-    explanation_df = explanations_to_df(exp_list)
+    explanation_df = explanations_to_df(exp_object_list)
     only_claim_predictions_df = explanations_to_df(only_claim_list)
     only_claim_predictions_df['type'] = 'only_claim'
 
@@ -269,7 +256,6 @@ def load_preprocess_explanations():
         mask = all_df['dataset_file_name'] == dataset_file_name
         all_df.loc[mask, 'label'] = all_df.loc[mask, 'id'].apply(lambda x: orig_records_dict[x]['label'])
 
-
     all_df.to_csv(os.path.join(xfc.experiment_definitions.C.RESULTS_DIR, 'all_exp.csv'), index=False)
 
     # assert (pd.Series(only_claim_predictions_df[id_cols].astype(str).apply('_'.join, 1).unique()).isin(explanation_df[
@@ -314,4 +300,22 @@ def load_experiment_result_by_code(experiment_code, results_path) -> list:
 
 
 if __name__ == '__main__':
-    load_preprocess_explanations()
+    load_preprocess_explanations(
+        experiment_code_list=[
+            'sk_f_jf_1.1',
+            'sk_f_jf_1.0',
+            'sk_f_jf_1.1b',
+            'sk_f_jf_1.1n',
+            'f_bs_1.0',
+            'f_bs_1.1',
+            'f_bs_1.1b',
+            'f_bs_1.1c',
+            'fbs_np_1.0',
+            'fbs_np_2.0',
+        ],
+        only_claim_exp_list=[
+            'oc_1.0',
+            'oc_1.1',
+            'oc_fbs_np_1.0',
+        ]
+    )
