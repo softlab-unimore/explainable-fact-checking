920143bd62e8e1685f7966ffdf96e83647860190
Andrea <baraldian@gmail.com>
2024-10-16 18:05:19 +0200

    fix: workarounds to make predictions somewhat properly; prm to specify script path for multiprocessing.

--- explainable_fact_checking/model_adapters/genFCExp.py
+++ explainable_fact_checking/model_adapters/genFCExp.py
@@ -8,7 +8,8 @@ import numpy as np
 
 class GenFCExp:
     blank_evidence_list = [
-        ' ', '.',
+        'Here is a blank evidence.',
+        'Hello.',
         'In general, most claims of this nature require thorough investigation before conclusions can be drawn.',
         'There is a consensus that such situations should be approached cautiously due to the uncertainty involved.',
         'As is commonly observed, various perspectives can emerge, each contributing to a broader discussion that might not immediately provide clarity.',
@@ -16,21 +17,45 @@ class GenFCExp:
         'While general principles may apply, it is important to recognize the uniqueness of each scenario and the intricacies involved.',
         'Without further corroboration, it is typically advised to approach such matters with caution, given the potential for misinterpretation or ambiguity.']
 
-    def __init__(self, model_path, random_seed=42):
+    def __init__(self, model_path, random_seed=42, script_name='test_model.sh', nlabels=None):
         self.model_path = model_path
         self.random_seed = random_seed
+        self.script_name = script_name
+        if nlabels is None:
+            nlabels = 3
+        self.nlabels = nlabels
 
     def predict(self, input_list, return_exp=False):
         # add blank evidence to the input list when the evidence is empty
-        old_evidence = []
+        old_len = len(input_list)
+        empty_mask = []
         i = 0
         for record in input_list:
-            old_evidence.append(copy.deepcopy(record['evidence']))
+            empty_mask.append(len(record['evidence']) == 0)
             if len(record['evidence']) == 0:
-                # record['evidence'] = self.blank_evidence_list[i % len(self.blank_evidence_list)]
                 i += 1
-                record['evidence'] = [' . ']
-        input_list += input_list[-2:]
+                idx = i % len(GenFCExp.blank_evidence_list)
+                record['evidence'] = self.blank_evidence_list[idx:idx + 2]
+            record.pop('input_txt_to_use', None)
+
+        if len(input_list) < 50:
+            missing = 50 - len(input_list)
+        else:
+            missing = 10
+        # idx_non_empty = empty_mask.index(False) if False in empty_mask else 0  if we want to use the first record as a template
+        fill_element = copy.deepcopy(input_list[0])
+        to_add = [copy.deepcopy(fill_element) for _ in range(missing)]
+        for i, el in enumerate(to_add):
+            idx = i % len(GenFCExp.blank_evidence_list)
+            el['evidence'] = el['evidence'] + [self.blank_evidence_list[idx]]
+        input_list = input_list + to_add  # do not use += here as it will modify the original list
+        for i,x in enumerate(input_list):
+            x['id'] = i
+            nev = len(x['evidence'])
+            if len(x['goldtag']) < nev:
+                x['goldtag'] = x['goldtag'] + [0] * (nev - len(x['goldtag']))
+            else:
+                x['goldtag'] = x['goldtag'][:nev]
 
         with tempfile.TemporaryDirectory() as tmpdirname:
             tmp_input = os.path.join(tmpdirname, 'tmp.json')
@@ -39,16 +64,21 @@ class GenFCExp:
                 json.dump(input_list, f)
             # run the model script
             os.system(
-                f'CUDA_VISIBLE_DEVICES={os.environ.get("CUDA_VISIBLE_DEVICES")} /home/bussotti/XFCresults/datasets_v2/pipelines/Isabelle/test_model.sh {tmp_input} {self.model_path} {tmp_out}')
+                f'CUDA_VISIBLE_DEVICES={os.environ.get("CUDA_VISIBLE_DEVICES")}'
+                f' /home/bussotti/XFCresults/datasets_v2/pipelines/Isabelle/{self.script_name}'
+                f' {tmp_input} {self.model_path} {tmp_out} {self.nlabels}')
             # read the output file
             with open(tmp_out, 'r') as f:
                 out_list = json.load(f)
-        input_list = input_list[:-2]
-        for record, ev in zip(input_list, old_evidence):
-            record['evidence'] = ev
+        out_list = [x[:old_len] for x in out_list]
+        input_list = input_list[:old_len]
+        for record, empty in zip(input_list, empty_mask):
+            if empty:
+                record['evidence'] = []
+
         if return_exp:
             return out_list
-        return np.array(out_list[0][:48])
+        return np.array(out_list[0])
 
     def __call__(self, *args, **kwargs):
         return self.predict(*args, **kwargs)
